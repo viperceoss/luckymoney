1f1766deb1d9155ad1168e2d56158909e7792ca9
viperceoss <326049128@qq.com>
2019-12-24 16:51:57 +0800

    1.0

---
+++ src/main/java/com/work/luckymoney/config/Config.java
@@ -0,0 +1,39 @@
+package com.work.luckymoney.config;
+
+import com.sun.org.apache.xpath.internal.objects.XString;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Component
+@ConfigurationProperties(prefix = "limit")
+public class Config {
+    private BigDecimal minMoney;
+    private BigDecimal maxMoney;
+    private String description;
+
+    public BigDecimal getMinMoney() {
+        return minMoney;
+    }
+
+    public void setMinMoney(BigDecimal minMoney) {
+        this.minMoney = minMoney;
+    }
+
+    public BigDecimal getMaxMoney() {
+        return maxMoney;
+    }
+
+    public void setMaxMoney(BigDecimal maxMoney) {
+        this.maxMoney = maxMoney;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
---
+++ src/main/java/com/work/luckymoney/controller/HelloController.java
@@ -0,0 +1,29 @@
+package com.work.luckymoney.controller;
+
+import com.work.luckymoney.config.Config;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @Autowired
+    private Config config;
+    @GetMapping("/hello/say")
+    public String say() {
+        return "江神无敌"+config.getDescription();
+    }
+
+    @GetMapping("/abc")
+    public int say2() {
+        return 2;
+    }
+
+    @GetMapping("/123")
+    public boolean abc() {
+        return true;
+    }
+
+
+}
